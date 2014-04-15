606f390f0f0bd58cd6c72ca15876728c22ebab2a
Sen <github@sen.cx>
2014-04-14 21:50:54 -0400

    Fixed bugs where multiple different speech items on one hook would get out of order and/or overwrite each other.

--- modules/speech.rb
+++ modules/speech.rb
@@ -3,8 +3,11 @@ module NestControl
     include Singleton
 
     def hooks(hook)
+      hook = hook.to_sym
+      
       @hooks = Hash.new if !@hooks
-      @hooks[hook] = SpeechHook.new(hook)
+      @hooks[hook] = SpeechHook.new(hook) if !@hooks[hook]
+      @hooks[hook]
     end
   end
 
@@ -13,11 +16,12 @@ module NestControl
       @name = name
       @log = Log4r::Logger['speech']
       @hook_callees = Hash.new
+      @log.debug "Initializing new speech hook '#{name}'"
     end
     
-    def register_with(priority, callee)
-      @log.info "Registering #{callee} with speech hook #{@name}"
-      @hook_callees["#{priority}#{callee}".to_sym] = callee
+    def register_with(priority, name, callee)
+      @log.info "Registering #{name} (#{callee}) with speech hook #{@name}"
+      @hook_callees["#{priority}#{name}".to_sym] = callee
     end
     
     # TODO: maybe speaker should be a configured attribute of the hook itself, somewhere?
@@ -26,11 +30,12 @@ module NestControl
       phrase = ""
       # go through everything registered and either add the static text or call the procedure
       # to generate dynamic text
-      @hook_callees.merge(NestConfig[:speech][:hook_speech][@name.to_sym]).each do |priority, item|
+      @hook_callees.merge(NestConfig[:speech][:hook_speech][@name.to_sym]).sort.map do |priority, item|
         if(item.class == String)
-          phrase += item
+          phrase += "#{item} "
         else
-          puts "Calling '#{item}' to generate speech"
+          @log.info "Calling '#{item}' to generate speech"
+          phrase += "#{item.call} "
         end
       end
       @log.debug "Saying \"#{phrase}\" for activated hook #{@name}"
