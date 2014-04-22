47b5df235c3433a760c3631305b02664f548a08d
Sen <github@sen.cx>
2014-04-22 18:17:17 -0400

    Fixing some display/logging issues with dependency resolution.

--- nestcontrol.rb
+++ nestcontrol.rb
@@ -9,32 +9,33 @@ require './handlers.rb'
 include NestControl
 basedir = File.dirname(__FILE__)
 
-# used by modules to depend on other modules/load them first
-def depends_on(mod)
-  puts "Loading #{mod} as a dependency"
-  require "#{File.dirname(__FILE__)}/modules/#{mod.to_s}.rb"
-end
-
 # set up logging
 Log4r::YamlConfigurator.decode_yaml(YAML.load_file("#{basedir}/etc/logging.yaml"))
-log = Log4r::Logger['nestcontrol']
+@log = Log4r::Logger['nestcontrol']
+
+# used by modules to depend on other modules/load them first
+def depends_on(mod)
+  @log.info "Loading module '#{mod}' as a dependency"
+  loaded = require "#{File.dirname(__FILE__)}/modules/#{mod.to_s}.rb"
+  @log.info "Module '#{mod}' already loaded, not re-loading" if !loaded
+end
 
 # load all the config files
 NestConfig::load_files("#{basedir}/etc/*.yaml")
 
 # load all the support classes
 Dir.glob("#{basedir}/lib/*.rb").each do |file|
-  log.info "Loading support library '#{File.basename(file, ".rb")}'"
+  @log.info "Loading support library '#{File.basename(file, ".rb")}'"
   load file
 end
 
 # load all the modules
 module_list = Dir.glob("#{basedir}/modules/*.rb").sort
 module_list.each do |file|
-  log.info "Loading module '#{File.basename(file, ".rb")}'"
+  @log.info "Loading module '#{File.basename(file, ".rb")}'"
   loaded = require file
-  log.info "Module '#{File.basename(file, ".rb")}' already loaded, not re-loading" if !loaded
+  @log.info "Module '#{File.basename(file, ".rb")}' already loaded, not re-loading" if !loaded
 end
 
-log.info "Initialization complete, system ready"
+@log.info "Initialization complete, system ready"
 CoreEventMachine.instance.run
