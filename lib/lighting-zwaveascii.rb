46179215635e1971ecdbd7340c1f51df38163179
sen <github@sen.cx>
2015-07-05 11:49:02 -0400

    Adding some support for zwave door locks.
    Currently actions upon locking/unlocking are hardcoded, need to move
    this to a config file.

--- lib/lighting-zwaveascii.rb
+++ lib/lighting-zwaveascii.rb
@@ -11,6 +11,7 @@ module NestControl
       @controller = nil
       begin
         @controller = ZWave::ASCII.new NestConfig[:zwave][:controller][:port], 115200
+	@controller.debug = true
       rescue Errno::ENOENT
         # TODO: retry later on
         log.error "Unable to initialize ZWave controller, ZWave devices will not function"
@@ -31,6 +32,9 @@ module NestControl
 	  when "scene_controller"
 	    log.info "Initializing ZWave device #{device_name} as a #{details[:buttons].count}-button #{details[:type]} with ID #{details[:id]}"
 	    @controllers[details[:id]] = details[:buttons]
+          when "door_lock"
+            log.info "Initializing ZWave device #{device_name} as a #{details[:type]} with ID #{details[:id]}"
+            @devices[device_name.to_s] = @controller.door_lock(details[:id])
           else
             log.error "Unable to initialize device #{device_name} as type #{details[:type]} is unknown"
         end
@@ -44,6 +48,10 @@ module NestControl
         @devices
       end
 
+      def reset
+        @controller.abort
+      end
+
       # get eventmachine to poll for events
       CoreEventMachine.instance.add_server self
     end
@@ -54,11 +62,25 @@ module NestControl
         EventMachine::PeriodicTimer.new(1) do
 	  events = @controller.fetch_events
 	  next if !events
+	  p events
 	  events.each do |event|
 	    if(event.class == ZWave::SceneActivationEvent) then
+	      # Scene Activation
 	      activate_item = @controllers[event.node][event.scene-1]
 	      puts "Trying to activate item #{activate_item} for scene #{event.scene} on node #{event.node}"
-	      Scenes::launch activate_item.to_sym
+	      if(!Scenes::launch(activate_item.to_sym)) then
+	        # no scene by that name, must be a device on/off button instead
+		puts "No scene found."
+	      end
+	    elsif(event.class == ZWave::NotificationEvent) then
+	      # Security Notification
+	      # FIXME: don't hardcode all this, and support multiple profiles for different PINs
+	      case event.type
+	        when :unlocked
+		  Scenes::launch :Home
+		when :locked
+		  Scenes::launch :Away
+	      end
 	    end
 	  end
 	end
